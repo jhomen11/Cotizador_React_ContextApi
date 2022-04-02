d779a9208712ced49f0f9b2872ebf0ee0f2f6be7
Jhonny <jhomen11@gmail.com>
2022-04-02 18:36:50 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>Hola</h1>
+      
+    </div>
+      
+  )
+}
+
+export default App
